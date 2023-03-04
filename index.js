f9840f088661a8144494d56ae6802bdd12d3843d
Philemon Forson <firstkoolaid21@gmail.com>
2023-03-04 12:25:36 +0000

    changed db.json

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const jsonServer = require('json-server')
 const cors = require('cors')
 const path = require('path')
+const AWS = require('aws-sdk');
+// const fs = require('fs');
+
+// const s3 = new AWS.S3();
+// const params = {
+//   Bucket: 'boardbucket',
+//   Key: 'data.json'
+// };
+// const file = fs.createWriteStream('/tmp/data.json');
+
+// s3.getObject(params)
+//   .createReadStream()
+//   .pipe(file)
+//   .on('error', function(err) {
+//     console.error(err);
+//   })
+//   .on('close', function() {
+//     console.log('File downloaded successfully.');
+//   });
+
 
 const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
+
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 server.use(router)
+
 
 const PORT = 8000
 server.listen(PORT, () => {
